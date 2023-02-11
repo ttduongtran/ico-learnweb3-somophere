466d8afb76c15abc9185b5eb44b23e9dfd016a9b
Duong Tran <ttduongtran@gmail.com>
2023-02-11 23:33:35 +0700

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb49248D834EB4d2a4fD6CAeA23a39C3aE2276169"
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
